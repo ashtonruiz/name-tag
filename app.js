1abeb37dfb32d2ff1160d9f22c15f496afa325e5
Ashton Ruiz <ashton.ruiz@yahoo.com>
2022-10-01 16:36:04 -0700

    secondary button added

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ console.log(nameDisplay);
 const updateBtn = document.getElementById('update-btn');
 updateBtn.addEventListener('click', () => {
     console.log('i am clicking the button');
-const nameInput = document.getElementById('name-input');
-nameDisplay.textContent = nameInput.value;
-}); 
+    const nameInput = document.getElementById('name-input');
+    nameDisplay.textContent = nameInput.value;
+});
+
+const locationName = document.getElementById('location');
+console.log(locationName);
+
+const updateBtn2 = document.getElementById('update-btn2');
+updateBtn2.addEventListener('click', () => {
+    console.log('i am clicking the button');
+    const locationInput = document.getElementById ('location-input');
+    locationName.textContent = locationInput.value;
+});
