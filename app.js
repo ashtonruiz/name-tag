f94fdac56f7b2a99cf4b59d1a229e1230de5154b
Ashton Ruiz <ashton.ruiz@yahoo.com>
2022-10-01 16:01:54 -0700

    added buttons and changed design

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 // grab DOM elements
 
 // set event listeners
+const nameDisplay = document.getElementById('name');
+console.log(nameDisplay);
+
+const updateBtn = document.getElementById('update-btn');
+updateBtn.addEventListener('click', () => {
+    console.log('i am clicking the button');
+const nameInput = document.getElementById('name-input');
+nameDisplay.textContent = nameInput.value;
+}); 
